bb21166243178889785db8de355a7042de881a95
Rene Gust <r.gust@reply.de>
2019-12-30 20:07:20 +0100

    removes comment

--- chihuahua/app.js
+++ chihuahua/app.js
@@ -36,7 +36,7 @@ addBtn.style.display = 'none';
         var scrollHeight = $(document).height();
         var scrollPosition = $(window).height() + $(window).scrollTop();
         if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
-            //fetchNextPosts();
+            fetchNextPosts();
         }
     }, true);
 
